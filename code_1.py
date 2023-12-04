7ef6925f3f4fa1065045b189de8b86c815417621
gor07ey <152075976+gor07ey@users.noreply.github.com>
2023-12-04 19:58:05 +0300

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,27 @@
+def main():
+
+def generate_random_data():
+
+    data = generate_random_data()
+    main()
+import random
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
+    return data
+if __name__ == "__main__":
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    for item in data:
+    data = generate_random_data()
+    return data
+
+
+        print(f"Random Number: {item}")
+
+    main()
+def main():
+if __name__ == "__main__":
